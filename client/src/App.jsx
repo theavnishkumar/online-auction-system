631c675d62b0c726265ca7089fb64191168151d1
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2024-07-27 14:34:48 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-semibold text-center underline">
+        Online Auction System
+      </h1>
+    </>
+  );
+}
+
+export default App;
