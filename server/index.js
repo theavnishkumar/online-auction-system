f3a92658a00846780677620e7cfe14dd4891d1ab
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-05 00:27:54 +0530

    added patch method to cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }));
 
