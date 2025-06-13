7428e5940860b222b2823b019970bac04f8983ea
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-13 23:51:00 +0530

    revert

--- client/src/layout/OpenLayout.jsx
+++ client/src/layout/OpenLayout.jsx
@@ -1,27 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Outlet } from "react-router";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import ScrollToTop from "../utils/ScrollToTop";
-import { useDispatch, useSelector } from "react-redux";
-import LoadingScreen from "../components/LoadingScreen";
-import { checkAuth } from "../store/auth/authSlice";
 
 export const OpenLayout = () => {
-  const dispatch = useDispatch();
-  const { loading, user } = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (!user && loading) {
-      dispatch(checkAuth());
-    }
-  }, [dispatch, user, loading]);
-
-  if (loading) return <LoadingScreen />;
-
   return (
     <>
-      <ScrollToTop />
+    <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
