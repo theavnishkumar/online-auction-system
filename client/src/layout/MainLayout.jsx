88bb56db98400d7549a7c767155f476ebd0dd350
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-15 00:36:42 +0530

    fix userSelector not defined Closes #5

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -4,6 +4,7 @@ import { Footer } from "../components/Footer";
 import LoadingScreen from "../components/LoadingScreen";
 import { useNavigate } from "react-router";
 import ScrollToTop from "../utils/ScrollToTop";
+import { useSelector } from "react-redux";
 
 export const MainLayout = () => {
   const { user, loading } = useSelector((state) => state.auth);
