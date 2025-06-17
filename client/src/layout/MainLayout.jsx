70369c9df56805ca24b15da47899d85ce5ace5f7
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-18 00:14:02 +0530

    fix: move navigate() into useEffect to fix MainLayout warning [closes #11]

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,23 +1,26 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import LoadingScreen from "../components/LoadingScreen";
-import { useNavigate } from "react-router";
 import ScrollToTop from "../utils/ScrollToTop";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export const MainLayout = () => {
   const { user, loading } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  if (!loading && !user) {
-    navigate("/login");
-    return null;
-  }
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    }
+  }, [loading, user, navigate]);
 
-  return loading ? (
-    <LoadingScreen />
-  ) : (
+  if (loading) return <LoadingScreen />;
+
+  if (!user) return null; // Prevents flashing protected content before redirect
+
+  return (
     <>
       <ScrollToTop />
       <Navbar />
